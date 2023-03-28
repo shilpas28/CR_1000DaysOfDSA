24ec45f59c791ba0319abe84d1584f2597ca6657
shilpas28 <69781857+shilpas28@users.noreply.github.com>
2023-03-28 21:00:51 +0530

    Day 28 - Problems

---
+++ Day28/Leetcode/MinimumCostForTickets.java
@@ -0,0 +1,30 @@
+//Leetcode - using DP
+//983. Minimum Cost For Tickets
+//Time complexity:
+//Space complexity:
+
+package Leetcode;
+
+public class MinimumCostForTickets {
+    
+    public static void main(String[] args) {
+        int[] days = {1,4,6,7,8,20};
+        int[] costs = {2,7,15};
+        System.out.println(mincostTickets(days, costs));
+    }
+
+    public static int mincostTickets(int[] days, int[] costs) {
+        int n = days.length;
+        int[] dp = new int[n+1];
+        for(int i = 0; i < n; ++i) dp[i] = 365*costs[0];
+        
+        for(int i = n-1; i >= 0; --i){
+            int d7 = i, d30 = i;
+            while(d7 < n && days[d7] < days[i] + 7) ++d7;
+            while(d30 < n && days[d30] < days[i] + 30) ++d30;
+            dp[i] = Math.min(costs[0] + dp[i+1], Math.min(costs[1] + dp[d7], costs[2] + dp[d30]));
+        }
+        return dp[0];
+    }
+
+}
