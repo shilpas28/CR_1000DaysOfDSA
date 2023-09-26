7e0037e44310db3afcd6bc6f6e061cea063a05fa
shilpas28 <69781857+shilpas28@users.noreply.github.com>
2023-09-26 09:56:46 +0530

    Day 206 - Problems

---
+++ Day206/GFG/FindAllFourSumNumbers.java
@@ -0,0 +1,58 @@
+//GFG
+//Find All Four Sum Numbers
+//Time complexity: O(N^3)
+//Space complexity: O(N^2)
+
+package GFG;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FindAllFourSumNumbers {
+
+    public static void main(String[] args) {
+        int K = 3;
+        int A[] = { 0, 0, 2, 1, 1 };
+        System.out.println(fourSum(A, K));
+    }
+
+    public static ArrayList<ArrayList<Integer>> fourSum(int[] arr, int k) {
+        // code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // HashSet<ArrayList<Integer>> hs = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && arr[i] == arr[i - 1])
+                continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j != i + 1 && arr[j] == arr[j - 1])
+                    continue;
+                int s = j + 1, e = n - 1;
+
+                while (s < e) {
+                    int sum = arr[i] + arr[j] + arr[s] + arr[e];
+                    if (sum == k) {
+                        ArrayList<Integer> l = new ArrayList<>(Arrays.asList(arr[i], arr[j], arr[s], arr[e]));
+                        // if(!hs.contains(l)){
+                        // list.add(l);
+                        // hs.add(l);
+                        // }
+                        list.add(l);
+                        s++;
+                        e--;
+                        while (s < e && arr[s] == arr[s - 1])
+                            s++;
+                        while (s < e && arr[e] == arr[e + 1])
+                            e--;
+                    } else if (sum < k)
+                        s++;
+                    else
+                        e--;
+                }
+            }
+        }
+        return list;
+    }
+}
