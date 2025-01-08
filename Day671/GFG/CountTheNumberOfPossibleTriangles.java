463998d4cb9f47cffb04c5de40eb72234acc8b5b
shilpas28 <69781857+shilpas28@users.noreply.github.com>
2025-01-08 10:25:57 +0530

    Day 671 - Problems

---
+++ Day671/GFG/CountTheNumberOfPossibleTriangles.java
@@ -0,0 +1,35 @@
+//GFG
+//Count the number of possible triangles
+//Time complexity: O(NlogN)
+//Space complexity: O(1) 
+
+import java.util.Arrays;
+
+public class CountTheNumberOfPossibleTriangles {
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 6, 3, 7 };
+        System.out.println(countTriangles(arr));
+    }
+
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                int sum = arr[l] + arr[r];
+                if (sum > arr[i]) {
+                    ans += (r - l);
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ Day671/Leetcode/CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,33 @@
+//Leetcode
+//3042. Count Prefix and Suffix Pairs I - Brute Force
+//Time complexity: O((n^2)*m)
+//Space complexity: O(1) 
+
+public class CountPrefixAndSuffixPairsI {
+
+    public static void main(String[] args) {
+        String[] words = { "a", "aba", "ababa", "aa" };
+        System.out.println(countPrefixSuffixPairs(words));
+    }
+
+    public static int countPrefixSuffixPairs(String[] words) {
+        int n = words.length;
+        int count = 0;
+        // Step 1: Iterate through each pair of words
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                String str1 = words[i];
+                String str2 = words[j];
+                // Step 2: Skip if the first string is larger than the second
+                if (str1.length() > str2.length())
+                    continue;
+                // Step 3: Check if str1 is both the prefix and suffix of str2
+                if (str2.startsWith(str1) && str2.endsWith(str1)) {
+                    ++count;
+                }
+            }
+        }
+        // Step 4: Return the total count of prefix-suffix pairs
+        return count;
+    }
+}
