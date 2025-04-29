799f409ece54f08d8003965793dfff4692414bf2
Shilpa <69781857+shilpas28@users.noreply.github.com>
2025-04-29 08:51:58 +0530

    Day 779 - Problems

---
+++ Day779/GFG/SortALinkedListOf0s1sAnd2s.java
@@ -0,0 +1,64 @@
+//GFG
+//Sort a linked list of 0s, 1s and 2s
+//Time complexity: O(N)
+//Space complexity: O(N)
+
+public class SortALinkedListOf0s1sAnd2s {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(1);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = new Node(0);
+        head.next.next.next.next.next.next = new Node(2);
+        head.next.next.next.next.next.next.next = new Node(2);
+        Node res = segregate(head);
+        printList(res);
+    }
+
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) {
+        // code here
+        int cnt[] = new int[3];
+        for (Node curr = head; curr != null; curr = curr.next) {
+            if (curr.data == 0)
+                cnt[0]++;
+            else if (curr.data == 1)
+                cnt[1]++;
+            else
+                cnt[2]++;
+        }
+        Node curr = head;
+        int idx = 0;
+        while (curr != null) {
+            if (cnt[idx] == 0)
+                idx++;
+
+            if (cnt[idx] != 0) {
+                curr.data = idx;
+                cnt[idx]--;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
---
+++ Day779/Leetcode/CountSubarraysWhereMaxElementAppearsAtLeastKTimes.java
@@ -0,0 +1,40 @@
+//Leetcode
+//2962. Count Subarrays Where Max Element Appears at Least K Times
+//Time complexity: O(N)
+//Space complexity: O(N)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountSubarraysWhereMaxElementAppearsAtLeastKTimes {
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 2, 3, 3 };
+        int k = 2;
+        System.out.println(countSubarrays(nums, k));
+    }
+
+    public static long countSubarrays(int[] nums, int k) {
+
+        int maxnum = Arrays.stream(nums).max().getAsInt();
+        long ans = 0;
+        int n = nums.length;
+
+        List<Integer> maxind = new ArrayList<>();
+        maxind.add(-1); // Initialize with -1 to handle edge case
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == maxnum) {
+                maxind.add(i);
+            }
+        }
+
+        int indsize = maxind.size();
+        for (int i = 1; i <= indsize - k; i++) {
+            long l = maxind.get(i) - maxind.get(i - 1) - 1;
+            long r = n - 1 - maxind.get(i + k - 1);
+            ans += (l + 1) * (r + 1);
+        }
+        return ans;
+    }
+}
