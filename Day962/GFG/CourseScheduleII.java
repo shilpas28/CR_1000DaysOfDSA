26e6d908d08af97d69d2083b993760b20d79da49
Shilpa <69781857+shilpas28@users.noreply.github.com>
2025-11-01 16:02:52 +0530

    Day 962 - Problems

---
+++ Day962/GFG/CourseScheduleII.java
@@ -0,0 +1,51 @@
+//GFG
+//Course Schedule II
+//Time complexity: O(n + m)
+//Space complexity: O(n + m)
+
+package GFG;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseScheduleII {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int prerequisites[][] = { { 1, 0 }, { 2, 1 } };
+        System.out.println(findOrder(n, prerequisites));
+    }
+
+    public static ArrayList<Integer> findOrder(int n, int[][] prerequisites) {
+        // code here
+        int c[] = new int[n]; // In degree Array
+        List<List<Integer>> edges = new ArrayList<>(); // adj array , store all the one direction edges
+        ArrayList<Integer> ans = new ArrayList<>(); // store the result
+        Queue<Integer> q = new LinkedList<>(); // Queue for going thorugh node for BFS Approch
+
+        for (int i = 0; i < n; i++)
+            edges.add(new ArrayList<>()); // Intialization
+        for (int i[] : prerequisites) {
+            edges.get(i[1]).add(i[0]); // Storing the edges
+            c[i[0]]++; // Increment the number if in Degree
+        }
+
+        for (int i = 0; i < n; i++)
+            if (c[i] == 0)
+                q.add(i); // If in degree is zero add it in the Queue
+
+        while (!q.isEmpty()) {
+            int e = q.poll(); // fetch top element form Queue
+            ans.add(e); // Add that in the result as top element have indegree always zero
+            for (int k : edges.get(e)) // go to all the edges connected to node
+                if (--c[k] == 0)
+                    q.add(k); // if in degress for the element is zero add that in the queue
+        }
+        
+        if (ans.size() != n)
+            return new ArrayList<Integer>(); // check if the size of answer is total no of edges means no loop exist
+        return ans;
+    }
+}
---
+++ Day962/Leetcode/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,89 @@
+//Leetcode
+//3217. Delete Nodes From Linked List Present in Array - Method 1
+//Time complexity: O(NlogN)
+//Space complexity: O(N)
+
+import java.util.Arrays;
+
+public class DeleteNodesFromLinkedListPresentInArray {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3 };
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode res = modifiedList(nums, head);
+        printList(res);
+    }
+
+    public static ListNode modifiedList(int[] nums, ListNode head) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        ListNode slow = head;
+        ListNode fast = slow.next;
+        while (fast != null) {
+            if (fast.val > nums[n - 1]) {
+                slow = fast;
+                fast = fast.next;
+                continue;
+            }
+            if (help(nums, fast.val)) {
+                slow.next = fast.next;
+                fast = fast.next;
+            } else {
+                slow = fast;
+                fast = fast.next;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (head.val == nums[i]) {
+                head = head.next;
+            }
+        }
+        return head;
+    }
+
+    public static boolean help(int[] nums, int a) {
+        int l = 0, h = nums.length - 1, mid = 0;
+        while (l <= h) {
+            mid = (l + h) / 2;
+            if (nums[mid] == a) {
+                return true;
+            } else if (nums[mid] > a) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null)
+                System.out.print("->");
+
+            head = head.next;
+        }
+    }
+}
